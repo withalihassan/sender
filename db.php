dc594f816063f6dde705e5186204484df1c420d9
withalihassan <macbookpro@Alis-MacBook-Pro.local>
2025-02-22 22:12:00 +0500

    db  file changed

--- db.php
+++ db.php
@@ -1,13 +1,13 @@
 <?php
 // db.php
-// $host     = 'localhost';
-// $dbname   = 'sns_db';
-// $username = 'root';
-// $password = '';
-$host     = 'srv1803.hstgr.io';
-$dbname   = 'u671407910_sendko';
-$username = 'u671407910_sendko';
-$password = 'Ali@@0990';
+$host     = 'localhost';
+$dbname   = 'sender';
+$username = 'sender';
+$password = 'Tech@#009';
+// $host     = 'srv1803.hstgr.io';
+// $dbname   = 'u671407910_sendko';
+// $username = 'u671407910_sendko';
+// $password = 'Ali@@0990';
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
