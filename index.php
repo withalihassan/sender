b8c82b336646c7e4a22892f5f8839550219f2054
withalihassan <macbookpro@Alis-MacBook-Pro.local>
2025-02-22 19:03:31 +0500

    Changes made

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 // Simple PHP page to test GitHub push
-echo "<h1>Hello, GitHub! IAM Here Again Meri jann</h1>";
+echo "<h1>Hello, Technomomo!</h1>";
 echo "<p>Current Date & Time: " . date("Y-m-d H:i:s") . "</p>";
 ?>
