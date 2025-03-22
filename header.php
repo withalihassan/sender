37ead44ca96feed2038a73cf4600b1f662e95cd2
withalihassan <macbookpro@Alis-MacBook-Pro.local>
2025-03-22 22:26:07 +0500

    Changes

--- header.php
+++ header.php
@@ -69,7 +69,7 @@
   <nav class="nav">
     <ul>
       <li><a href="index.php">Home</a></li>
-      <li><a href="./sendko/" target="_blank">Home</a></li>
+      <li><a href="./sendko/" target="_blank">Sendko</a></li>
       <li><a href="number_dir.php">Numbers Directory</a></li>
       <li><a href="my_numbers.php">My Numbers</a></li>
       <li><a href="ac_summary.php">Ac Summary</a></li>
