4c2a3056dc787791db711908b69754806420b178
withalihassan <macbookpro@Alis-MacBook-Pro.local>
2025-05-24 09:02:38 +0500

    Changes

--- deploy.php
+++ deploy.php
@@ -1,4 +1,9 @@
 <?php
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 
 $repoDir = '/var/www/html';
 $repoUrl = 'https://github.com/withalihassan/sender.git';
