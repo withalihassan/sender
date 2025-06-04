26159af3c6f4cf7f9d3e99d3ac6dca282d6bfe9e
withalihassan <macbookpro@Alis-MacBook-Pro.local>
2025-06-04 22:13:32 +0500

    Changes

--- iam_users.php
+++ iam_users.php
@@ -96,7 +96,7 @@ if (isset($_POST['submit'])) {
                 </thead>
                 <tbody>
                     <?php
-                    $stmt_iam = $pdo->query("SELECT * FROM iam_users WHERE by_user='$session_id' AND added_by='boys' ORDER BY id DESC");
+                    $stmt_iam = $pdo->query("SELECT * FROM iam_users WHERE by_user='$session_id' AND added_by='boys' ORDER BY created_at");
                     while ($row_iam_users = $stmt_iam->fetch(PDO::FETCH_ASSOC)) {
                         // Update by_user for this iam_users record
                         // $iam_user_id = $row_iam_users['id'];
