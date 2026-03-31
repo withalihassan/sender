d6c8ff13c2768efaa98f947d8a3004c1da040381
withalihassan <macbookpro@Alis-MacBook-Pro.local>
2026-03-31 14:34:29 +0500

    all 31 march2

--- webhook.php
+++ webhook.php
@@ -3,8 +3,7 @@
 $output = [];
 $return = 0;
 
-exec('/bin/bash /var/www/sender/update.sh 2>&1', $output, $return);
+exec('/bin/bash /usr/local/bin/sender-update.sh 2>&1', $output, $return);
 
 http_response_code($return === 0 ? 200 : 500);
-
 echo implode("\n", $output);
